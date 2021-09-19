136d93980dab0dbe6f13e952a1c81505f361c6c7
Elizabeth Aufzien <eliauf23@gmail.com>
2021-09-18 21:16:48 -0400

    implemented helper functions + inc. documentation in .h file

--- c_textsearch.c
+++ c_textsearch.c
@@ -19,6 +19,11 @@ int main(int argc, char **argv)
 {
   FILE *input;
 
+      //TODO: remove - debugging print statement
+  printf("\nargv[0] = %s, argv[1] = %s, argv[2] = %s, argv[3] = %s\n\n", argv[0], argv[1], argv[2], argv[3]);
+
+
+
   //exit with error code 1 if there's error w number of user args
   if ((argc != 3) && (argc != 4))
   {
@@ -33,24 +38,26 @@ int main(int argc, char **argv)
   if (input == NULL)
   {
     fprintf(stderr, "File cannot be opened. ");
+    fclose(input);
     exit(2);
   }
 
   //determine whether user wants lines to be printed
-  int printOccurrences = (argc == 3);
+  int printOccurrences = (argc == 3)? 1:0;
 
   //TODO: remove - debugging print statement
   printf("\nargc = %d, printOccurrences = %d\n\n", argc, printOccurrences);
 
   int num_occurrences = find_all_occurrences(input, argv[argc - 2], printOccurrences);
-  fclose(input);
   //if !printOccurrences -> argv == 4
-  if (!printOccurrences && strings_equal(argv[1], "-c"))
+  if (printOccurrences == 0 && strings_equal(argv[1], "-c"))
   {
     //print num occurrences instead
     fprintf(stdout, "%d occurrence(s)", num_occurrences);
   }
 
   //program ran successfully
+  fclose(input);
+
   exit(0);
 }
--- textsearch_fns.h
+++ textsearch_fns.h
@@ -3,17 +3,114 @@
 
 #define MAXLINE 511
 
-// TODO: declare helper functions here (to be implemented in
-// c_textsearch_fns.c and asm_textsearch_fns.S)
 
+/*
+ * Description: Puts characters from text file until encounters newline or EOF.
+ * 
+ *
+ * Parameters:
+ *   in - File pointer.
+ *   buf - character buffer to store line.
+ *
+ * Returns:
+ *   0 - EOF
+ *   1 - has next line = true
+ */
 int read_line(FILE *in, char *buf);
-void print_line(FILE *out, const char *buf);
-unsigned count_occurrences(const char *line, const char *str);
-unsigned find_string_length(const char *s);
-int starts_with(const char *s, const char *pfx);
-int strings_equal(const char *s1, const char *s2);
-//char * get_substr(const char * line, int index, int len);
-unsigned count_occ_in_file(FILE* fileptr, char* search, int argc);
 
+
+/*
+ * Description: Prints line of text contained in buf.
+ * 
+ *
+ * Parameters:
+ *   out - output file/output stream
+ *   buf - buffer with line to print
+ *   
+ */
+void print_line(FILE *out, const char *buf);
+
+
+/*
+ * Description: Count instances of search string in line from text file.
+ * 
+ *
+ * Parameters:
+ *   line - line from text file.
+ *   str - target string to match.
+ *
+ * Returns:
+ *   number of occurrences of target string
+ */
+unsigned count_occurrences(const char *line, const char *str);
+
+/*
+ * Description: Counts number of characters up to null terminator.
+ * 
+ *
+ * Parameters:
+ *   s - string.
+ *
+ * Returns:
+ *   length of string s
+ */
+unsigned find_string_length(const char *s);
+
+
+/*
+ * Description: Determines if string starts with a certain prefix.
+ * 
+ *
+ * Parameters:
+ *   s - string.
+ *   pfx - prefix
+ *
+ * Returns:
+ *   0 - false: s does not start with pfx
+ *   1 - true: s starts with pfx
+ */
+int starts_with(const char *s, const char *pfx);
+
+/*
+ * Description: Checks equality of two strings.
+ * 
+ *
+ * Parameters:
+ *   s1 - string 1.
+ *   s2 - string 2.
+ * Returns:
+ *   0 - if not equal
+ *   1 - if equal
+ */
+int strings_equal(const char *s1, const char *s2);
+
+/*
+ * Description: Find number of occurrences in entire text file (and print if argc=3)
+ * 
+ *
+ * Parameters:
+ *   in - file ptr.
+ *   search - search string
+ *   printOccurrences - boolean: if 1, then print matching lines when you find them. 
+ *
+ * Returns:
+ *   unsigned num of occurrences
+ */
+unsigned find_all_occurrences(FILE* in, char* search, int printOccurrences);
+
+/*
+ * Description:
+ * 
+ *
+ * Parameters:
+ *   line - line from text file
+ *   str_len - length of substring
+ *   i - index that substring begins at
+ *
+ * Returns:
+ *   desc return val
+ */
+
+char *get_substr(const char *line, int str_len, int i);
 
 #endif // TEXTSEARCH_FNS_H
