09e8cce591efc237eb48efc1edfcdf5d1bd9fc55
Elizabeth Aufzien <eaufzie1@jh.edu>
2021-10-06 17:35:30 -0400

    fixed issue in c_textsearch.c where we close a null file pointer & updated textsearch_fns to check for this circumstance

--- c_textsearch.c
+++ c_textsearch.c
@@ -2,67 +2,54 @@
 #include <stdlib.h>
 #include "textsearch_fns.h"
 
-/*
- * Description: Main function
- *
- * Parameters:
- *   argc - num. of user provided args 
- *   argv - user provided args
- *
- * Returns:
- *   0 - if program successfully executes.
- *   1 - if user input is invalid or file cannot be opened
- */
 int main(int argc, char **argv)
 {
-    FILE *input;
-    char *search = argv[argc - 2];
-
-    int filename_idx = argc - 1;
-
-    //error types - check down below
-    int too_few_args = argc < 3;
-    int too_many_args = argc > 4;
-    int has_c_flag = strings_equal(argv[1], "-c");
-
     //initial error handling & try to open file
-    if (too_few_args)
+    if (argc < 3)
     {
         fprintf(stderr, "User did not supply sufficient arguments.");
         exit(1);
-    } else if(too_many_args) {
+    }
+    else if (argc > 4)
+    {
         fprintf(stderr, "User supplied too many arguments.");
         exit(1);
     }
     else
     {
+
+        FILE *input;
+        char *search = argv[argc - 2];
+        char *filename = argv[argc - 1];
+
+        int has_c_flag = strings_equal(argv[1], "-c");
         //open user-specified text file to read from
-        input = fopen(argv[filename_idx], "r");
+        input = fopen(filename, "r");
 
         if (input == NULL)
         {
-            fprintf(stderr, "File cannot be opened. ");
-            fclose(input);
+            fprintf(stderr, "File cannot be opened.\n");
             exit(1);
         }
-    }
 
-    if (argc == 3 && !has_c_flag)
-    {
-        //1 = set print occurrences to true
-        find_all_occurrences(input, search, 1);
-    }
-    else if (argc == 4)
-    {
-        //0 = set print occurrences to false
-        printf("%d occurrence(s)\n", find_all_occurrences(input, search, 0));
-    } else {
-        fprintf(stderr, "Error!");
-        exit(1);
-    }
+        if (argc == 3 && !has_c_flag)
+        {
+            //1 = set print occurrences to true
+            find_all_occurrences(input, search, 1);
+        }
+        else if (argc == 4)
+        {
+            //0 = set print occurrences to false
+            printf("%d occurrence(s)\n", find_all_occurrences(input, search, 0));
+        }
+        else
+        {
+            fprintf(stderr, "Error!");
+            exit(1);
+        }
 
-
-    //program ran successfully
-    fclose(input);
-    exit(0);
+        //program ran successfully
+        fclose(input);
+        exit(0);
+    }
 }
